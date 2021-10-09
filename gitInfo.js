13e5e118e89ebb83770ebc401088bac8b787f8b3
Omar Abdullah <Malfucious97@gmail.com>
2021-10-09 17:56:41 -0500

    Finished Assessment

---
+++ gitInfo.js
@@ -0,0 +1,78 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+const gitDefinition = 'Git is used to track files and make necessary changes in the system more easily. '
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+const gitHubDefinition = 'Github is used to make reviews and collaborations. From the website it can be edited and fix or create content'
+//////////////////PROBLEMS 3 - 8////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
+
+//CODE HERE
+const init = {description:"Primarily used to create a new Git repository or convert existing repository", code:"git init"}
+//////////////////PROBLEM 4////////////////////
+/*
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
+*/
+
+//CODE HERE
+const clone = {description:"Used to copy a repo and put it in another location.", code:"git clone"}
+//////////////////PROBLEM 5////////////////////
+/*
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
+*/
+
+//CODE HERE
+const status = {description:"Status shows if any changes have been made and which files aren't being tracked by git", code:"git status"}
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+//CODE HERE
+const add = {description:"Add current content to existing paths. However you can choose how much you want to add whether it be parts or as a whole.", code:"git add"}
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+//CODE HERE
+const commit = {description:"Commit's can be messages,data,timestamps, etc... which can detail the journey of the repository", code:"git commit --m"}
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+
+//CODE HERE
+const push = {description:"Pushing transports changes to your remote repository, as well as push local repository to remote repo", code:"git push"}
---
+++ sldc.js
@@ -0,0 +1,40 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+Planning
+Analysis of Requirements
+Design
+Implementation
+Integration
+Maintenance
+
+// Analysis of Requirements
+// Design
+// Implementation
+// Maintain
+// Planning
+// Testing & Integration
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/const planning = "All projects start with planning. The primary objective to observe potential issues and figure out solutions along the route. More effective the planning can mean the elimination of majority of problems."
+
+
+const analysisOfRequirements = "Analysis focuses on the needs and conditions to meet to new or altered project"
+
+
+const design = "Design paints the layout, functions , and basically details the overall outline of the application or operation"
+
+
+const implementation = "Implementation will implement and put it into action. Specifically the designs,functions etc... to come together"
+
+
+const integration = "The overall implementation will be integrated into the environment which will eventually be installed"
+
+
+const maintenance = "Developers will test the activities on the site and see if any bugs or issues are on the end user side. As they run maintenance tests they can also implement fixes after deployment to ensure it runs smoothly"
